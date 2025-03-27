373567c19290f144f24fbbfc9fa1c4adc0a48577
Mainca123 <giangchinhtgp@gmail.com>
2025-03-27 14:51:37 +0700

    First commit

--- src/main/java/com/example/BaiTapVN2/repository/EmployeesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BaiTapVN2.repository;
-
-import com.example.BaiTapVN2.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeesRepository extends JpaRepository<Employee, Long>{}
